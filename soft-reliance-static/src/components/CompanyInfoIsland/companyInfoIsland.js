70e69ddb2ec081253a0bb5ac8d1abe49dcca2418
anilgolani <anil.golani@outlook.com>
2020-10-11 17:07:25 +0000

    info island

--- soft-reliance-static/src/components/CompanyInfoIsland/companyInfoIsland.js
+++ soft-reliance-static/src/components/CompanyInfoIsland/companyInfoIsland.js
@@ -6,8 +6,9 @@ export default () => {
 	return (
 		<div className="info-island-container">
 			<div className="info-island">
-				hello
-
+				<h2>Expertise in enterprise solutions</h2>
+				<p>Unleashing the power of innovative Information Technology solutions in Ghana</p>
+				<a href="/services" className="styledButton">View All Services</a>
 				<div className="block-element" />
 			</div>
 		</div>
