b160c1b23831eacaa020d0844005513b7d8a1e00
anilgolani <anil.golani@outlook.com>
2020-10-11 12:13:04 +0000

    info island container design

---
+++ soft-reliance-static/src/components/CompanyInfoIsland/companyInfoIsland.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './companyInfoIsland.css';
+
+export default () => {
+	return (
+		<div className="info-island-container">
+			<div className="info-island">
+				hello
+
+				<div className="block-element" />
+			</div>
+		</div>
+	)
+}
---
+++ soft-reliance-static/src/components/Homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './homepage.css';
+import CompanyInfoIsland from '../CompanyInfoIsland/companyInfoIsland.js';
+
+export default () => {
+	return (
+		<div className="homepage-container">
+		    <CompanyInfoIsland />
+		</div>
+	)
+}
